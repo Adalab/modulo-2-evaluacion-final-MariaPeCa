c5f9a4234a92e79b24a8c85662c83f2f2b5b2037
MariaPeCa <mpenacantero@gmail.com>
2023-06-11 16:58:43 +0200

    improve render function with conditional option with placeholder

--- public/assets/js/main 2.js
+++ public/assets/js/main 2.js
@@ -13,21 +13,21 @@
 //Comenzaremos copiandonos los datos del API (el array de objetos) a una variable de nuestro código JavaScript y generando el HTML para un único personaje.
 
 const ulElement = document.querySelector('.js_card_list');
-const url = `https://dev.adalab.es/api/disney?pageSize=15`;
+const url = `https://api.disneyapi.dev/character?pageSize=50`;
 
 
 
 let listCharachtersApi = []; //la relleno cuando me lleguen los datos del servidor
 
-//crear var para cuando no haya img
-
-
 //fetch
 fetch(url)
     .then((response) => response.json())
     .then((listData) => {
+        console.log(listData);
         listCharachtersApi = listData.data
-        renderAllCharacters(listCharachtersApi);   
+        renderAllCharacters(listCharachtersApi);
+        //ulElement.innerHTML = renderOneCharacter(data.data[0]);
+        
     });
 
 //funciones
@@ -38,15 +38,11 @@ function renderAllCharacters(listData) {
 }
 
 function renderOneCharacter(character) {
-    let img = character.imageUrl;
-    if (img === "") {
-        img = "https://via.placeholder.com/210x295/ffffff/555555/?text=Disney";
-    }
     let html = `
         <li id="${character._id}" class="card">
         <article class="character__box">
         <img class="character__img js_img"
-            src="${img}"
+            src="${character.imageUrl}"
             alt="Disney Characters:${character.name}" />
         <p class="character__name js_name">"${character.name}"</p>
         </article>
@@ -56,7 +52,7 @@ function renderOneCharacter(character) {
 }
 //Después, añadiremos un bucle para que se genere el HTML de cada personaje que hay en el array.
 
-//hacer un if para cuando las tarjetas notengan img...
+
 
 
 //eventos
