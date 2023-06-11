a96479803dd6133bc1bd324d6796f280f309ecf9
MariaPeCa <mpenacantero@gmail.com>
2023-06-11 20:32:55 +0200

    improve conditional in function renderOneCharacter

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 //PASOS
 //hacer el HTML básico (crear una tarjeta de ejemplo, estilo)
 //pintar una tarjeta (fetch, crear la función que renderiza la tarjeta)
@@ -9,41 +7,50 @@
 //añadir a mis paletas favoritas (identificar )
 
 
-//variables
+//VARIABLES
 //Comenzaremos copiandonos los datos del API (el array de objetos) a una variable de nuestro código JavaScript y generando el HTML para un único personaje.
 
 const ulElement = document.querySelector('.js_card_list');
-const url = `https://dev.adalab.es/api/disney?pageSize=15`;
+const url = `https://api.disneyapi.dev/character?pageSize=50`;
 
 
 
-let listCharachtersApi = []; //la relleno cuando me lleguen los datos del servidor
+let listCharactersApi = []; //la relleno cuando me lleguen los datos del servidor
+let listCharactersFav = [];//crear el array de favoritos, aquí debo guardar el eelmento clicado
 
-//crear var para cuando no haya img
-
-
-//fetch
+//fetch peticion al servidor
 fetch(url)
     .then((response) => response.json())
     .then((listData) => {
-        listCharachtersApi = listData.data
-        renderAllCharacters(listCharachtersApi);   
+        listCharactersApi = listData.data //elijo la propiedad del array que necesito y lo guardo en array personal
+        renderAllCharacters(listCharactersApi);  //una vez tengo los datos los renderizo(hago q se vean)
+    
     });
 
-//funciones
+//FUNCIONES
+//creo una función para renderizar los datos que tengo guardados en mi array. esta función recorre el array y por cada elemento del array guarda en el HTML lo q nos retorna la funcion renderOneCharacter (creada previamente, que es un solo una tarjeta personaje)
 function renderAllCharacters(listData) {
     for (const character of listData) {
         ulElement.innerHTML += renderOneCharacter(character);
     }
+    addEventCharacter();
 }
 
+function addEventCharacter(){
+const liElementList = document.querySelectorAll('.js_list');
+    for (const li of liElementList) {
+        li.addEventListener("click", handelClick);
+    }
+}
+
+//Crear función render, esta función nos retorna un li con todo el HTML correspondiente a un personaje de la lista de la API. Posteriormente le añado un id(atributo gancho) para poder identificar el elemento clicado posteriormente. Añado un condicional para mostrar un placeholder el caso de que la tarjeta proporcionada por la Api no tenga imagen
 function renderOneCharacter(character) {
     let img = character.imageUrl;
-    if (img === "") {
+    if (img === undefined || img === "") {
         img = "https://via.placeholder.com/210x295/ffffff/555555/?text=Disney";
     }
     let html = `
-        <li id="${character._id}" class="card">
+        <li id="${character._id}" class="card js_list">
         <article class="character__box">
         <img class="character__img js_img"
             src="${img}"
@@ -54,9 +61,24 @@ function renderOneCharacter(character) {
            
     return html;
 }
-//Después, añadiremos un bucle para que se genere el HTML de cada personaje que hay en el array.
+//EVENTOS
+//escuchar un evento click sobre cada uno de las tarjetas de personajes para obtener la info del atributo gancho y saber cuál es el elemento clicado. Se lo he puesto al li completo por eso uso currentTarget.
+function handelClick (event){
+    const id = parseInt(event.currentTarget.id); //parseInt para convertirlo en número
 
-//hacer un if para cuando las tarjetas notengan img...
+
+//method find
+const selectedCharacter = listCharactersApi.find((item) => item._id === id);
+//
+
+listCharactersFav.push(selectedCharacter);
+console.log(listCharactersFav);
+}
+
+//Una vez obtengo el id me interesa guardar todo ese objeto que obtengo de la tarjeta de personajes en un array nuevo. El array de todos mis favoritos. Primero crear el array
 
 
-//eventos
+
+
+
+
